0ce35fe27bcae458dacabcf6fabb508fd26ce1ec
patryk77k <patryk77.k@gmail.com>
2023-04-19 01:49:55 +0200

    img added

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home_social">
+      <a
+        href="http://www.instagram.com"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="http://www.instagram.com"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-dribbble"></i>
+      </a>
+      <a
+        href="http://www.instagram.com"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
